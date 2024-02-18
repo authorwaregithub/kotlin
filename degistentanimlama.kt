bc80ba98c607cd1637042ea2d180e596a8774b10
Postuldak <123419955+postuldak@users.noreply.github.com>
2024-02-18 23:43:22 +0300

    Create degistentanimlama.kt

---
+++ degistentanimlama.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>) {
+
+    // Değişken tanımlama işleminde çıktı olarak : "Merhaba ben Postuldak" verisini göreceğiz.
+
+    var name: String
+    name = "Postuldak"
+
+    println("Merhaba ben : " + name)
+}
